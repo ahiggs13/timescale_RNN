3ddf4f5dd849818e7155f8e90e27cd86e0525505
Raiyyan Siddiqui <raiahmsid@gmail.com>
2026-05-04 12:57:25 -0400

    fixed submit_expts.sh and updated analysis notebook

--- submit_expts.sh
+++ submit_expts.sh
@@ -19,19 +19,19 @@ SEED=$(( (SLURM_ARRAY_TASK_ID + OFFSET) % N_SEEDS ))
 CONFIG=${CONFIGS[$CONFIG_IDX]}
 
 BASENAME=$(basename $CONFIG .yaml)
-AUTOCORR=$(echo $BASENAME | grep -oP 'autocorr\K[0-9]*\.?[0-9]+')
-KERNEL_TAU=$(echo $BASENAME | grep -oP 'ktau\K[0-9]*\.?[0-9]+')
-DELAY=$(echo $BASENAME      | grep -oP 'delay\K[0-9]*\.?[0-9]+')
+AUTOCORR=$(echo $BASENAME  | grep -oP 'autocorr\K[0-9]+\.?[0-9]*')
+KERNEL_TAU=$(echo $BASENAME | grep -oP 'ktau\K[0-9]+\.?[0-9]*')
+DELAY=$(echo $BASENAME      | grep -oP 'delay\K[0-9]+\.?[0-9]*')
 
 if echo $BASENAME | grep -q 'normal'; then
-    ETA=$(echo $BASENAME | grep -oP 'eta\K[0-9]*\.?[0-9]+')
+    ETA=$(echo $BASENAME | grep -oP 'eta\K[0-9]+\.?[0-9]*')
     NAME="autocorr${AUTOCORR}_ktau${KERNEL_TAU}_delay${DELAY}_normal_eta${ETA}_seed${SEED}"
 elif echo $BASENAME | grep -q 'groups'; then
-    GROUPS_TAU=$(echo $BASENAME | grep -oP 'groups\K[0-9]*\.?[0-9]+')
-    PROP=$(echo $BASENAME       | grep -oP 'prop\K[0-9]*\.?[0-9]+')
+    GROUPS_TAU=$(echo $BASENAME | grep -oP 'groups\K[0-9]+\.?[0-9]*')
+    PROP=$(echo $BASENAME       | grep -oP 'prop\K[0-9]+\.?[0-9]*')
     NAME="autocorr${AUTOCORR}_ktau${KERNEL_TAU}_delay${DELAY}_groups${GROUPS_TAU}_prop${PROP}_seed${SEED}"
 else
-    VANILLA_TAU=$(echo $BASENAME | grep -oP 'vanilla\K[0-9]*\.?[0-9]+')
+    VANILLA_TAU=$(echo $BASENAME | grep -oP 'vanilla\K[0-9]+\.?[0-9]*')
     NAME="autocorr${AUTOCORR}_ktau${KERNEL_TAU}_delay${DELAY}_vanilla${VANILLA_TAU}_seed${SEED}"
 fi
 
@@ -40,3 +40,8 @@ mv logs/tmp_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.err logs/${NAME}.err
 
 echo "Config: $CONFIG | Seed: $SEED | Name: $NAME"
 python -u run_expt.py --config=$CONFIG --seed=$SEED --name=$NAME
+
+# sbatch submit_expts.sh 0
+# sbatch submit_expts.sh 1000
+# sbatch submit_expts.sh 2000
+# sbatch submit_expts.sh 3000
